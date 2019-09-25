2058751a6c61c3658d9bbe46c5183c3449606899
karoliuakas <38911258+karoliuakas@users.noreply.github.com>
2019-09-25 10:22:30 +0300

    Add files via upload
    pakeistos kabutes

--- 2uzd/2uzd.rb
+++ 2uzd/2uzd.rb
@@ -1,10 +1,10 @@
-puts "Įveskite egzamino bala"
+puts 'Įveskite egzamino bala'
 a = gets.to_i
 
 if a >= 5 && a<11 
-	puts "Egzaminas išlaikytas"
+	puts 'Egzaminas išlaikytas'
 elsif a<5 && a>0
-	puts "Egzaminas neišlaikytas"
+	puts 'Egzaminas neišlaikytas'
 else
-	puts "tokio pazymio negali buti"
+	puts 'tokio pazymio negali buti'
 end
