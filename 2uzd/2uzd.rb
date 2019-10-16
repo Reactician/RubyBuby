d9e2bb9b35307a510134d6def526e79057c65d31
karoliuakas <38911258+karoliuakas@users.noreply.github.com>
2019-10-16 09:37:45 +0300

    Add files via upload

--- 2uzd/2uzd.rb
+++ 2uzd/2uzd.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 puts 'Įveskite egzamino bala'
 a = gets.to_i
 
-if a >= 5 && a<11 
-	puts 'Egzaminas išlaikytas'
-elsif a<5 && a>0
-	puts 'Egzaminas neišlaikytas'
+if a >= 5 && a < 11
+  puts 'Egzaminas išlaikytas'
+elsif a < 5 && a > 0
+  puts 'Egzaminas neišlaikytas'
 else
-	puts 'tokio pazymio negali buti'
+  puts 'tokio pazymio negali buti'
 end
