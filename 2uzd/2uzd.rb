b8124d05bf837df5b26ca96aecf2e08822415f7e
karoliuakas <38911258+karoliuakas@users.noreply.github.com>
2019-10-16 09:51:15 +0300

    Add files via upload

--- 2uzd/2uzd.rb
+++ 2uzd/2uzd.rb
@@ -5,7 +5,7 @@ a = gets.to_i
 
 if a >= 5 && a < 11
   puts 'Egzaminas išlaikytas'
-elsif a < 5 && a > 0
+elsif a < 5 && a.positive?
   puts 'Egzaminas neišlaikytas'
 else
   puts 'tokio pazymio negali buti'
