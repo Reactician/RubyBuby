7e0436698826d99cab799a09e9b424b7a41d72d9
karoliuakas <38911258+karoliuakas@users.noreply.github.com>
2019-10-16 09:36:10 +0300

    Add files via upload

--- 1uzd/1uzd.rb
+++ 1uzd/1uzd.rb
@@ -1,9 +1,11 @@
-masyvas= []
+# frozen_string_literal: true
+
+masyvas = []
 
 while masyvas.length < 3
-  puts "iveskite #{masyvas.length+1}-aji skaiciu"
+  puts "iveskite #{masyvas.length + 1}-aji skaiciu"
   i = gets.chomp
 
   masyvas << i
 end
-puts 'maziausias skaicius yra:'  + masyvas.min
+puts 'maziausias skaicius yra:' + masyvas.min
