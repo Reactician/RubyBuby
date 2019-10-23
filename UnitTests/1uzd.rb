336fa3d4f8bbde91a84012fe147ef76fe117d663
karoliuakas <38911258+karoliuakas@users.noreply.github.com>
2019-10-23 09:57:19 +0300

    changes for rubocop

--- UnitTests/1uzd.rb
+++ UnitTests/1uzd.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 require 'test/unit'
-
+# CLass for 1st program
 class Pirma
-  def masyvuks(a, b, c)
-    masyvas = [a, b, c]
+  def masyvuks(pirmasskaicius, antrasskaicius, treciasskaicius)
+    masyvas = [pirmasskaicius, antrasskaicius, treciasskaicius]
 
     masyvas.min
   end
 end
 print 'Maziausias elementas:'
 puts Pirma.new.masyvuks(1, 2, 3)
-
+# Test class for 1st program
 class TestPirma < Test::Unit::TestCase
   def test_simple
     assert_equal(69, Pirma.new.masyvuks(69, 420, 852))
--- UnitTests/2uzd.rb
+++ UnitTests/2uzd.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
 require 'test/unit'
-
+# Class for 2nd program
 class Antra
-  def Egzas(a)
-    if a >= 5 && a < 11
+  def egzas(egzobalas)
+    if egzobalas >= 5 && egzobalas < 11
       'Egzaminas išlaikytas'
-    elsif a < 5 && a.positive?
+    elsif egzobalas < 5 && egzobalas.positive?
       'Egzaminas neišlaikytas'
     else
       'tokio pazymio negali buti'
+    end
   end
-end
 end
 
 puts 'Įveskite egzamino bala'
-a = gets.to_i
-puts Antra.new.Egzas(a)
-
+egzobalas = gets.to_i
+puts Antra.new.egzas(egzobalas)
+# Test for 2nd program
 class TestAntra < Test::Unit::TestCase
   def test_islaikytas
-    assert_equal('Egzaminas išlaikytas', Antra.new.Egzas(5))
+    assert_equal('Egzaminas išlaikytas', Antra.new.egzas(5))
   end
 
   def test_neislaikytas
-    assert_equal('Egzaminas neišlaikytas', Antra.new.Egzas(2))
-   end
+    assert_equal('Egzaminas neišlaikytas', Antra.new.egzas(2))
+  end
 
   def test_neratokio
-    assert_equal('tokio pazymio negali buti', Antra.new.Egzas(-5))
+    assert_equal('tokio pazymio negali buti', Antra.new.egzas(-5))
   end
 end
